4d05f72568b2f31207bac0ba48a76612dc249ec2
gongjun <gj2021@ZY-jinan>
2022-05-12 13:41:02 +0800

    提交透明去边框悬浮球项目

---
+++ App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using GalaSoft.MvvmLight.Threading;
+using System;
+using System.Windows;
+using WPFFloatBall.View;
+
+namespace WPFFloatBall
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            DispatcherHelper.Initialize();
+        }
+
+        private void ApplicationStartup(object sender, StartupEventArgs e)
+        {
+            try
+            {
+                FloatAlarmWIndow floatAlarmWIndow = new FloatAlarmWIndow();
+                floatAlarmWIndow.Show();
+            }
+            catch (Exception ex) {  }
+        }
+    }
+}
---
+++ View/FloatBAllWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace WPFFloatBall.View
+{
+    /// <summary>
+    /// FloatAlarmWIndow.xaml 的交互逻辑
+    /// </summary>
+    public partial class FloatAlarmWIndow : Window
+    {
+        public FloatAlarmWIndow()
+        {
+            InitializeComponent();
+
+        }
+
+    }
+}
